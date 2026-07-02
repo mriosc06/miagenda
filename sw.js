a67cb3d38d1569a608961cd6a7c386f196b29ad1
mriosc06 <mrioscontreras06@gmail.com>
2026-07-02 00:41:24 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = 'miagenda-v1';
+const ASSETS = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
+
+self.addEventListener('notificationclick', e => {
+  e.notification.close();
+  e.waitUntil(clients.openWindow('./index.html'));
+});
